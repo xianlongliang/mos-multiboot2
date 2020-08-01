52106bac6fb21eed6d29f2712d58d944d29dfa5d
Mario Lau <liuminghao233@gmail.com>
2020-08-01 22:34:44 +0800

    ..

--- kernel/std/stdint.h
+++ kernel/std/stdint.h
@@ -2,7 +2,7 @@
 
 #define NO_ALIGNMENT __attribute__((packed))
 
-typedef signed char int8_t;
+typedef char int8_t;
 typedef signed short int int16_t;
 typedef signed int int32_t;
 typedef signed long long int int64_t;
@@ -11,4 +11,5 @@ typedef unsigned short int uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long int uint64_t;
 typedef long unsigned int size_t;
+typedef long signed int ssize_t;
 
