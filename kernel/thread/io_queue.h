a5adfdebef1a22467d36d08c7b2120387b262750
Mario Lau <liuminghao233@gmail.com>
2020-08-01 22:35:57 +0800

    add io_queue

---
+++ kernel/thread/io_queue.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <std/stdint.h>
+#include <std/list.h>
+#include <std/lock_guard.h>
+#include "mutex.h"
+#include "task.h"
+#include "semaphore.h"
+#include "condition_variable.h"
+
+template <class T>
+class IOQueue
+{
+public:
+    IOQueue(uint64_t max_queue_size) : max_queue_size(max_queue_size), queue() {}
+
+    void Push(T &&val)
+    {
+        LockGuard lg(this->lock);
+        if (this->queue.size() > this->max_queue_size) {
+             return;
+        }
+        this->queue.push_front(forward<T>(val));
+        this->cv.notify_one();
+    }
+
+    T Pop()
+    {
+        LockGuard lg(this->lock);
+        this->cv.wait(this->lock, [this]() { return !this->Empty(); });
+        auto back = this->queue.back();
+        this->queue.pop_back();
+        return back;
+    }
+
+    bool Empty() { return this->queue.empty(); }
+
+private:
+    Mutex lock;
+    ConditionVariable cv;
+    uint64_t max_queue_size;
+    list<T> queue;
+};
