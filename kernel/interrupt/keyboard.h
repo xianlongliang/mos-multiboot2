9876fec43e6e917685fe7f8f28f86b8719aafb23
Mario Lau <liuminghao233@gmail.com>
2020-08-01 22:33:45 +0800

    add keyboardio

--- kernel/interrupt/keyboard.h
+++ kernel/interrupt/keyboard.h
@@ -1,4 +1,13 @@
-#pragma once 
+#pragma once
 #include <std/stdint.h>
+#include <thread/io_queue.h>
+#include <std/singleton.h>
 
 void keyboard_irq_handler(uint64_t error_code, uint64_t rsp, uint64_t rflags, uint64_t rip);
+
+class KeyboardIO : public Singleton<KeyboardIO>
+{
+public:
+    KeyboardIO() : Queue(64) {}
+    IOQueue<int8_t> Queue;
+};
