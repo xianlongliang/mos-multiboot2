1f21215f570c3f91bb8c49fdb986dda5cf5633f6
Mario Lau <liuminghao233@gmail.com>
2020-08-01 22:32:05 +0800

    idle task schedule logic

--- kernel/thread/scheduler.cpp
+++ kernel/thread/scheduler.cpp
@@ -1,29 +1,43 @@
 #include "scheduler.h"
 #include <std/printk.h>
 
+extern task_struct *init_task;
+
 void Scheduler::Schedule()
 {
     auto next = this->next_task;
     this->next_task = (task_struct *)list_next(&next->list);
-    printk("from %d to %d\n", current->pid, next->pid);
+    // next == current only when idle task is running
     if (next == current)
         return;
+    // printk("from %d to %d\n", current->pid, next->pid);
     switch_to(current, next);
 }
 
 Scheduler *Scheduler::Add(task_struct *task)
 {
-    if (this->next_task == nullptr) this->next_task = task;
+    if (this->next_task == nullptr)
+        this->next_task = task;
     list_add_to_behind(&current->list, &task->list);
     return this;
 }
 
 Scheduler *Scheduler::Remove(task_struct *task)
 {
+
+    auto next = this->next_task = (task_struct *)list_next(&current->list);
     // if current is removed, the next task should be updated
-    if (current == task) {
-        this->next_task = (task_struct *)list_next(&current->list);
+    if (current == task)
+    {
+        this->next_task = next;
     }
+
+    // if there's not task to run, add idle task back
+    if (current == next)
+    {
+        this->next_task = init_task;
+    }
+
     list_del(&task->list);
     return this;
 }
