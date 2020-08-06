5007df1ab5bb4a7e0861dd97674c4c20f53c4501
Mario Lau <liuminghao233@gmail.com>
2020-08-06 23:57:33 +0800

    ..

--- kernel/gdt.cpp
+++ kernel/gdt.cpp
@@ -3,8 +3,11 @@
 #include <std/printk.h>
 #include "tss.h"
 #include <std/singleton.h>
+#include <memory/kmalloc.h>
+#include <smp/cpu.h>
+#include <std/cpuid.h>
 
-static inline void load_gdt(struct GDT::GDTPointer *p)
+static inline void load_gdt(struct gdt_struct::GDTPointer *p)
 {
     asm("lgdt %0" ::"m"(*p));
 }
@@ -14,28 +17,45 @@ static inline void load_tr(uint16_t tr)
     asm volatile("ltr %0" ::"m"(tr));
 }
 
-void GDT::set_gdt_tss(int n, void *tss_addr, uint16_t limit, uint16_t attr)
+struct cpuid_struct
 {
-    auto addr = reinterpret_cast<uint64_t>(tss_addr);
-    struct GDT_TSS *ts = (struct GDT_TSS *)(&gdt_table[n]);
+    uint32_t rax;
+    uint32_t rbx;
+    uint32_t rcx;
+    uint32_t rdx;
+};
 
-    ts->limit = limit; // 103
-    ts->low[0] = (uint8_t)(addr);
-    ts->low[1] = (uint8_t)(addr >> 8);
-    ts->low[2] = (uint8_t)(addr >> 16);
-    ts->attr = attr;
-    ts->high[0] = (uint8_t)(addr >> 24);
-    ts->high[1] = (uint8_t)(addr >> 32);
-    ts->high[2] = (uint8_t)(addr >> 40);
-    ts->high[3] = (uint8_t)(addr >> 48);
-    ts->high[4] = (uint8_t)(addr >> 56);
+static cpuid_struct cpuid(uint32_t main_op)
+{
+    int a, b, c, d;
+    asm volatile(
+        "mov %0, %%eax \n\t"
+        "cpuid      \n\t"
+        "mov %%eax, %0\n\t"
+        "mov %%ebx, %1\n\t"
+        "mov %%ecx, %2\n\t"
+        "mov %%edx, %3\n\t"
+        : "=r"(a), "=r"(b), "=r"(c), "=r"(d)
+        : "0"(main_op)
+        : "rax", "rbx", "rcx", "rdx");
+    return {a, b, c, d};
 }
+
+// void GDT::Init()
+// {
+//     tss_init();
+//     set_gdt_tss(7, &get_tss(), 103, 0x89);
+//     load_gdt(&gdt_ptr);
+//     load_tr(0x38);
+//     printk("gdt_init GDT_PTR %x\n", &gdt_ptr);
+// }
 
 void GDT::Init()
 {
-    tss_init();
-    set_gdt_tss(7, &get_tss(), 103, 0x89);
-    load_gdt(&gdt_ptr);
+    auto cpuid_struct = cpuid(0x1);
+    auto local_apic_id = cpuid_struct.rbx >> 24;
+    auto cpu_struct = CPU::GetInstance()->Get(local_apic_id);
+    load_gdt(&cpu_struct.gdt.gdt_ptr);
     load_tr(0x38);
     printk("gdt_init GDT_PTR %x\n", &gdt_ptr);
 }
--- kernel/gdt.h
+++ kernel/gdt.h
@@ -6,11 +6,34 @@
 #define CODE_SEG 0x0008
 #define DATA_SEG 0x0010
 
+struct gdt_struct
+{
+
+    struct NO_ALIGNMENT GDTPointer
+    {
+        uint16_t limit;
+        void *gdt_address;
+    };
+    
+    uint64_t gdt_table[10] = {
+        0x0000000000000000,
+        0x0020980000000000, // KERNEL CODE
+        0x0000920000000000, // KERNEL DATA
+        0x0000000000000000,
+        0x0000000000000000,
+        0x0000f20000000000, // USER DATA
+        0x0020f80000000000, // USER CODE
+        // ... tss (16 bytes)
+    };
+
+    GDTPointer gdt_ptr = {uint16_t(80 - 1), gdt_table};
+};
+
 class GDT : public Singleton<GDT>
 {
 public:
     void Init();
-
+    void InitSMP();
     struct NO_ALIGNMENT GDTPointer
     {
         uint16_t limit;
@@ -29,8 +52,6 @@ private:
         // ... tss (16 bytes)
     };
 
-    GDTPointer gdt_ptr = {uint16_t(80 - 1), gdt_table};
-
-    void set_gdt_tss(int n, void *tss_addr, uint16_t limit, uint16_t attr);
+    gdt_struct::GDTPointer gdt_ptr = {uint16_t(80 - 1), gdt_table};
 
 };
