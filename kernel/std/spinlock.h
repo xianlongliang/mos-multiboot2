bc9fc1b44f14ebd3c8f3f9031e323d334515fde5
Mario Lau <liuminghao233@gmail.com>
2020-08-07 14:18:40 +0800

    smp spinlock

--- kernel/std/spinlock.h
+++ kernel/std/spinlock.h
@@ -1,44 +1,30 @@
 #pragma once
+
 #include <std/stdint.h>
 
 class Spinlock
 {
 public:
-
     inline void lock()
     {
-        asm volatile("1:	\n\t"
-                     "lock	decq	%0	\n\t"
-                     "jz	3f	\n\t"     // jump if lock == 0, exit the loop
-                     "2:	\n\t"         // if lock != 0
-                     "pause	\n\t"         // pause cpu
-                     "cmpq	$0,	%0	\n\t" // check if lock still == 0
-                     "jle	2b	\n\t"     // if true, pause cpu again
-                     "jmp	1b	\n\t"     // or we try to acquire the lock all the way back again
-                     "3:	\n\t"
+        auto addr = &this->lock_val;
+        asm volatile("movq $1, %rcx	            \n\t");
+        asm volatile("spin_lock_retry:          \n\t");
+        asm volatile("lock; cmpxchgq %%rcx, %0  \n\t"
                      : "=m"(this->lock_val)
-                     :
-                     : "memory");
-    }
-    
-    inline void unlock()
-    {
-        asm volatile("movq	$1,	%0	\n\t"
-                     : "=m"(this->lock_val)
-                     :
-                     : "memory");
+                     : "a"(0):"memory");
+        asm volatile("pause");
+        asm volatile("jnz    spin_lock_retry    \n\t");
     }
 
-    inline uint64_t trylock()
+    inline void unlock()
     {
-        uint64_t tmp_value = 0;
-        asm volatile("xchgq	%0,	%1	\n\t"
-                     : "=q"(tmp_value), "=m"(this->lock_val)
-                     : "0"(0)
+        asm volatile("lock; movq	$0,	%0	\n\t"
+                     : "=m"(this->lock_val)
+                     :
                      : "memory");
-        return tmp_value;
     }
 
 private:
-    volatile uint64_t lock_val = 1; // 1:unlock, 0:lock
+    volatile uint64_t lock_val = 0; // 1:unlock, 0:lock
 };
