13f0226e5cec038d5b9a0fa16ce20495c0d1ea86
Mario Lau <liuminghao233@gmail.com>
2020-08-12 15:05:04 +0800

    add atomic

--- kernel/thread/semaphore.h
+++ kernel/thread/semaphore.h
@@ -3,15 +3,19 @@
 #include <std/stdint.h>
 #include <std/list.h>
 #include <thread/task.h>
+#include <std/atomic.h>
 
 class Semaphore
 {
 public:
-    Semaphore(uint8_t value) : value(value) {}
+    Semaphore(uint8_t value) : value(value) {
+        printk("%p\n", &this->value);
+        int j = 1;
+    }
 
     void Down();
     void Up();
 private:
-    volatile uint8_t value;
+    atomic<uint8_t> value;
     list<task_struct *> wait_list;
 };
