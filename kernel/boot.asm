a259e5775d88da561026a413dbffefd8055480c6
Mario Lau <liuminghao233@gmail.com>
2020-08-16 13:58:20 +0800

    add sse

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -101,6 +101,15 @@ SMP_JMP:
     or ebx,0x80000001                 ; - by enabling paging and protection simultaneously.
     mov cr0, ebx
 
+    ;now enable SSE and the like
+    mov eax, cr0
+    and ax, 0xFFFB		;clear coprocessor emulation CR0.EM
+    or ax, 0x2			;set coprocessor monitoring  CR0.MP
+    mov cr0, eax
+    mov eax, cr4
+    or ax, 3 << 9		;set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+    mov cr4, eax
+
     lgdt [GDT.Pointer] 
     
     jmp CODE_SEG:smp_protect_mode
@@ -161,6 +170,15 @@ _start:
 
     mov eax, KERNEL_CR0
     mov cr0, eax
+
+    ;now enable SSE and the like
+    mov eax, cr0
+    and ax, 0xFFFB		;clear coprocessor emulation CR0.EM
+    or ax, 0x2			;set coprocessor monitoring  CR0.MP
+    mov cr0, eax
+    mov eax, cr4
+    or ax, 3 << 9		;set CR4.OSFXSR and CR4.OSXMMEXCPT at the same time
+    mov cr4, eax
 
     lgdt [GDT.Pointer]
 
