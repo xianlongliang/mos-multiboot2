26bddcf8e0ee7231916e2e1a07603f9037656104
Mario Lau <liuminghao233@gmail.com>
2020-08-22 22:58:49 +0800

    ..

--- kernel/kernel.cpp
+++ kernel/kernel.cpp
@@ -25,24 +25,8 @@ extern "C" void Kernel_Main(void *mbi_addr)
 {
   clear();
   basic_init(mbi_addr);
-  kmalloc_init();
   RSDT::GetInstance()->Init();
+  kmalloc_init();
   SMP::GetInstance()->Init();
-  unordered_set<int> uset;
-  int key = 1;
-  uset.insert(1);
-  uset.insert(2);
-  uset.insert(3);
-  uset.insert(4);
-  uset.insert(5);
-  uset.insert(6);
-  uset.insert(7);
-  uset.insert(8);
-  uset.insert(9);
-  uset.insert(10);
-  uset.insert(11);
-  uset.insert(12);
-  uset.insert(7);
-  uset.find(key);
   task_init();
 }
