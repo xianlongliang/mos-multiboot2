9be9a239088f3feaca40219f704d6e72458325c4
Mario Lau <liuminghao233@gmail.com>
2020-09-01 02:11:48 +0800

    ..

--- kernel/std/unique_ptr.h
+++ kernel/std/unique_ptr.h
@@ -3,38 +3,79 @@
 #include "noncopyable.h"
 #include "move.h"
 
-template <typename T>
-class unique_ptr : noncopyable
+namespace std
 {
-public:
-    unique_ptr(T *p) : ptr(p) {}
-    ~unique_ptr() { delete ptr; }
-
-    unique_ptr(unique_ptr<T> &&rv)
+    template <typename T>
+    class unique_ptr : noncopyable
     {
-        this->ptr = rv.ptr;
-        rv.ptr = nullptr;
+    public:
+        unique_ptr(T *p = nullptr) : ptr(p) {}
+
+        unique_ptr(unique_ptr<T> &&rhs)
+        {
+            this->ptr = rhs.ptr;
+            rhs.ptr = nullptr;
+        }
+
+        ~unique_ptr() { delete ptr; }
+
+        T *operator->() { return this->ptr; }
+
+        T &operator*() { return *ptr; }
+
+        T &operator[](int idx)
+        {
+            return ptr[idx];
+        }
+
+        T operator[](int idx) const
+        {
+            return ptr[idx];
+        }
+
+        unique_ptr<T> operator=(unique_ptr<T> &&rhs)
+        {
+            this->reset();
+            this->ptr = rhs.ptr;
+            rhs.ptr = nullptr;
+        }
+
+        explicit operator bool()
+        {
+            return this->ptr != nullptr;
+        }
+
+        T *get()
+        {
+            return this->ptr;
+        }
+
+        T *release()
+        {
+            auto p = this->ptr;
+            this->ptr = nullptr;
+            return p;
+        }
+
+        void reset(T *p = nullptr)
+        {
+            delete this->ptr;
+            this->ptr = p;
+        }
+
+    private:
+        T *ptr;
+    };
+
+    template <typename T, typename... Args>
+    unique_ptr<T> make_unique(Args &&... args)
+    {
+        return unique_ptr<T>(new T(forward<Args>(args)...));
     }
 
-    T *operator->() { return this->ptr; }
-    T &operator*() { return *ptr; }
-    
-    T &operator[](int idx)
+    template <class T>
+    void swap(unique_ptr<T> &a, unique_ptr<T> &b) noexcept
     {
-        return ptr[idx];
+        swap(a.ptr, b.ptr);
     }
-
-    T operator[](int idx) const
-    {
-        return ptr[idx];
-    }
-
-private:
-    T *ptr;
-};
-
-template <typename T, typename... Args>
-unique_ptr<T> make_unique(Args &&... args)
-{
-    return unique_ptr<T>(new T(forward<Args>(args)...));
-}
+}; // namespace std
