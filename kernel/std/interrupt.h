fc92cb57ca02598b65c3f77c4e3b79919718d75c
Mario Lau <liuminghao233@gmail.com>
2020-09-01 22:31:53 +0800

    add CLI_GUARD

--- kernel/std/interrupt.h
+++ kernel/std/interrupt.h
@@ -8,6 +8,12 @@ inline void sti() {
     asm volatile("sti");
 }
 
+#define CLI_GUARD(exp) \
+            cli(); \
+            exp;   \
+            sti() \
+            
+
 inline void hlt() {
     asm volatile("hlt");
 }
