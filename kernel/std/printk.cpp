5e2705f4a820df7641384d39014f28de5eaea682
Mario Lau <liuminghao233@gmail.com>
2020-09-07 00:14:52 +0800

    update gop

--- kernel/std/printk.cpp
+++ kernel/std/printk.cpp
@@ -2,154 +2,151 @@
 #include "port_ops.h"
 #include "spinlock.h"
 #include <memory/physical.h>
+#include <display/gop.h>
 
 static Spinlock printk_spinlock;
 
-namespace Kernel::VGA
-{
-        // VGA 的显示缓冲的起点是 0xB8000
-        static uint16_t *video_memory = (uint16_t *)(Phy_To_Virt(0xb8000));
+// namespace Kernel::VGA
+// {
+//         // VGA 的显示缓冲的起点是 0xB8000
+//         static uint16_t *video_memory = (uint16_t *)(Phy_To_Virt(0xc0000000));
 
-        // 屏幕"光标"的坐标
-        static uint8_t cursor_x = 0;
-        static uint8_t cursor_y = 0;
+//         // 屏幕"光标"的坐标
+//         static uint8_t cursor_x = 0;
+//         static uint8_t cursor_y = 0;
 
-        static void move_cursor()
-        {
-                // 屏幕是 80 字节宽
-                uint16_t cursorLocation = cursor_y * 80 + cursor_x;
+//         static void move_cursor()
+//         {
+//                 // 屏幕是 80 字节宽
+//                 uint16_t cursorLocation = cursor_y * 80 + cursor_x;
 
-                // 在这里用到的两个内部寄存器的编号为14与15，分别表示光标位置
-                // 的高8位与低8位。
+//                 // 在这里用到的两个内部寄存器的编号为14与15，分别表示光标位置
+//                 // 的高8位与低8位。
 
-                outb(0x3D4, 14);                  // 告诉 VGA 我们要设置光标的高字节
-                outb(0x3D5, cursorLocation >> 8); // 发送高 8 位
-                outb(0x3D4, 15);                  // 告诉 VGA 我们要设置光标的低字节
-                outb(0x3D5, cursorLocation);      // 发送低 8 位
-        }
+//                 outb(0x3D4, 14);                  // 告诉 VGA 我们要设置光标的高字节
+//                 outb(0x3D5, cursorLocation >> 8); // 发送高 8 位
+//                 outb(0x3D4, 15);                  // 告诉 VGA 我们要设置光标的低字节
+//                 outb(0x3D5, cursorLocation);      // 发送低 8 位
+//         }
 
-        void console_clear()
-        {
-                uint8_t attribute_byte = (0 << 4) | (15 & 0x0F);
-                uint16_t blank = 0x20 | (attribute_byte << 8);
+//         void console_clear()
+//         {
+//                 uint8_t attribute_byte = (0 << 4) | (15 & 0x0F);
+//                 uint16_t blank = 0x20 | (attribute_byte << 8);
 
-                int i;
-                for (i = 0; i < 80 * 25; i++)
-                {
-                        video_memory[i] = blank;
-                }
+//                 int i;
+//                 for (i = 0; i < 80 * 25; i++)
+//                 {
+//                         video_memory[i] = blank;
+//                 }
 
-                cursor_x = 0;
-                cursor_y = 0;
-                move_cursor();
-        }
+//                 cursor_x = 0;
+//                 cursor_y = 0;
+//                 move_cursor();
+//         }
 
-        static void scroll()
-        {
-                // attribute_byte 被构造出一个黑底白字的描述格式
-                uint8_t attribute_byte = (0 << 4) | (15 & 0x0F);
-                uint16_t blank = 0x20 | (attribute_byte << 8); // space 是 0x20
+//         static void scroll()
+//         {
+//                 // attribute_byte 被构造出一个黑底白字的描述格式
+//                 uint8_t attribute_byte = (0 << 4) | (15 & 0x0F);
+//                 uint16_t blank = 0x20 | (attribute_byte << 8); // space 是 0x20
 
-                // cursor_y 到 25 的时候，就该换行了
-                if (cursor_y >= 25)
-                {
-                        // 将所有行的显示数据复制到上一行，第一行永远消失了...
-                        int i;
+//                 // cursor_y 到 25 的时候，就该换行了
+//                 if (cursor_y >= 25)
+//                 {
+//                         // 将所有行的显示数据复制到上一行，第一行永远消失了...
+//                         int i;
 
-                        for (i = 0 * 80; i < 24 * 80; i++)
-                        {
-                                video_memory[i] = video_memory[i + 80];
-                        }
+//                         for (i = 0 * 80; i < 24 * 80; i++)
+//                         {
+//                                 video_memory[i] = video_memory[i + 80];
+//                         }
 
-                        // 最后的一行数据现在填充空格，不显示任何字符
-                        for (i = 24 * 80; i < 25 * 80; i++)
-                        {
-                                video_memory[i] = blank;
-                        }
+//                         // 最后的一行数据现在填充空格，不显示任何字符
+//                         for (i = 24 * 80; i < 25 * 80; i++)
+//                         {
+//                                 video_memory[i] = blank;
+//                         }
 
-                        // 向上移动了一行，所以 cursor_y 现在是 24
-                        cursor_y = 24;
-                }
-        }
+//                         // 向上移动了一行，所以 cursor_y 现在是 24
+//                         cursor_y = 24;
+//                 }
+//         }
 
-        void console_putc_color(char c, Color back, Color fore)
-        {
-                uint8_t back_color = (uint8_t)back;
-                uint8_t fore_color = (uint8_t)fore;
+//         void console_putc_color(char c, Color back, Color fore)
+//         {
+//                 uint8_t back_color = (uint8_t)back;
+//                 uint8_t fore_color = (uint8_t)fore;
 
-                uint8_t attribute_byte = (back_color << 4) | (fore_color & 0x0F);
-                uint16_t attribute = attribute_byte << 8;
+//                 uint8_t attribute_byte = (back_color << 4) | (fore_color & 0x0F);
+//                 uint16_t attribute = attribute_byte << 8;
 
-                // 0x08 是退格键的 ASCII 码
-                // 0x09 是tab 键的 ASCII 码
-                if (c == 0x08 && cursor_x)
-                {
-                        cursor_x--;
-                        video_memory[cursor_y * 80 + cursor_x] = ' ' | attribute;
-                }
-                else if (c == 0x09)
-                {
-                        cursor_x = (cursor_x + 8) & ~(8 - 1);
-                }
-                else if (c == '\r')
-                {
-                        cursor_x = 0;
-                        cursor_y++;
-                }
-                else if (c == '\n')
-                {
-                        cursor_x = 0;
-                        cursor_y++;
-                }
-                else if (c >= ' ')
-                {
-                        video_memory[cursor_y * 80 + cursor_x] = c | attribute;
-                        cursor_x++;
-                }
+//                 // 0x08 是退格键的 ASCII 码
+//                 // 0x09 是tab 键的 ASCII 码
+//                 if (c == 0x08 && cursor_x)
+//                 {
+//                         cursor_x--;
+//                         video_memory[cursor_y * 80 + cursor_x] = ' ' | attribute;
+//                 }
+//                 else if (c == 0x09)
+//                 {
+//                         cursor_x = (cursor_x + 8) & ~(8 - 1);
+//                 }
+//                 else if (c == '\r')
+//                 {
+//                         cursor_x = 0;
+//                         cursor_y++;
+//                 }
+//                 else if (c == '\n')
+//                 {
+//                         cursor_x = 0;
+//                         cursor_y++;
+//                 }
+//                 else if (c >= ' ')
+//                 {
+//                         video_memory[cursor_y * 80 + cursor_x] = c | attribute;
+//                         cursor_x++;
+//                 }
 
-                // 每 80 个字符一行，满80就必须换行了
-                if (cursor_x >= 80)
-                {
-                        cursor_x = 0;
-                        cursor_y++;
-                }
+//                 // 每 80 个字符一行，满80就必须换行了
+//                 if (cursor_x >= 80)
+//                 {
+//                         cursor_x = 0;
+//                         cursor_y++;
+//                 }
 
-                // 如果需要的话滚动屏幕显示
-                scroll();
+//                 // 如果需要的话滚动屏幕显示
+//                 scroll();
 
-                // 移动硬件的输入光标
-                move_cursor();
-        }
+//                 // 移动硬件的输入光标
+//                 move_cursor();
+//         }
 
-        void console_write(char *cstr)
-        {
-                while (*cstr)
-                {
-                        console_putc_color(*cstr++, Color::rc_black, Color::rc_white);
-                }
-        }
+//         void console_write(char *cstr)
+//         {
+//                 while (*cstr)
+//                 {
+//                         console_putc_color(*cstr++, Color::rc_black, Color::rc_white);
+//                 }
+//         }
 
-        void console_write_color(char *cstr, Color back, Color fore)
-        {
-                while (*cstr)
-                {
-                        console_putc_color(*cstr++, back, fore);
-                }
-        }
-} // namespace Kernel::VGA
+//         void console_write_color(char *cstr, Color back, Color fore)
+//         {
+//                 while (*cstr)
+//                 {
+//                         console_putc_color(*cstr++, back, fore);
+//                 }
+//         }
+// } // namespace Kernel::VGA
 
 void putchar(char ch)
 {
-        Kernel::VGA::console_putc_color(ch);
+        GOP::PutChar(ch);
 }
 
 void putvalue(char *val)
 {
-        while (*val != '\0')
-        {
-                Kernel::VGA::console_putc_color(*val);
-                ++val;
-        }
+        GOP::PutString(val);
 }
 
 char *int2hexstr(uint64_t val, int &end_index)
@@ -191,7 +188,7 @@ void putvalue(uint64_t val)
         auto buff = int2str(val, end_idx);
         for (int i = end_idx; i >= 0; --i)
         {
-                Kernel::VGA::console_putc_color(buff[i]);
+                GOP::PutChar(buff[i]);
         }
 }
 
@@ -199,7 +196,7 @@ void putvalue(int64_t val)
 {
         if (val < 0)
         {
-                Kernel::VGA::console_putc_color('-');
+                GOP::PutChar('-');
                 putvalue(uint64_t(-val));
                 return;
         }
@@ -207,26 +204,28 @@ void putvalue(int64_t val)
         putvalue(uint64_t(val));
 }
 
-void putvalue(uint8_t*val)
+void putvalue(uint8_t *val)
 {
         int end_idx;
         auto buff = int2hexstr(uint64_t(val), end_idx);
 
-        Kernel::VGA::console_putc_color('0');
-        Kernel::VGA::console_putc_color('x');
+        GOP::PutChar('0');
+        GOP::PutChar('x');
+
         auto padding = 16 - end_idx;
         while (padding--)
         {
-                Kernel::VGA::console_putc_color('0');
+                GOP::PutChar('0');
         }
         for (int i = end_idx; i >= 0; --i)
         {
-                Kernel::VGA::console_putc_color(buff[i]);
+                GOP::PutChar(buff[i]);
         }
 }
+
 void clear()
 {
-        Kernel::VGA::console_clear();
+        GOP::Clear();
 }
 
 void printk(const char *format, ...)
@@ -278,7 +277,7 @@ void printk(const char *format, ...)
                         case 'p':
                         {
                                 ++pformat;
-                                putvalue(va_arg(args, uint8_t*));
+                                putvalue(va_arg(args, uint8_t *));
                                 break;
                         }
                         default:
@@ -299,5 +298,4 @@ void printk(const char *format, ...)
 
         asm volatile("popf");
         printk_spinlock.unlock();
-
 }
